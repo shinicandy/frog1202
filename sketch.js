89bfb4309721c5784948e90ee375c976e369a66c
shinicandy <75354052+shinicandy@users.noreply.github.com>
2020-12-02 14:06:43 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://shinicandy.github.io/frog1202/model.json';
 
 
 function preload() {
